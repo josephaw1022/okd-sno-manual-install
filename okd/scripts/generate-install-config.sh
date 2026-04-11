5ff25a6a65f1d972ac7d43fac8addc4d9dcbe793
josephaw1022 <josephsims1@gmail.com>
2026-04-11 15:44:57 -0400

    Remove Cilium manifest generation and use OVNKubernetes

--- okd/scripts/generate-install-config.sh
+++ okd/scripts/generate-install-config.sh
@@ -42,7 +42,7 @@ networking:
     hostPrefix: 20
   machineNetwork:
   - cidr: $MACHINE_NETWORK_CIDR
-  networkType: None
+  networkType: OVNKubernetes
   serviceNetwork:
   - 172.30.0.0/16
 platform:
